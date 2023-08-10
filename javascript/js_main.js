23b36dbe51a474bf264d2a0d21c162759f662c0e
Epersonal <92110978+EvanM07@users.noreply.github.com>
2023-08-10 16:17:40 -0700

    Making general changes
    Made changes to the CSS for the Buttons, Navbar, and Dropdown Menus so everything is working as it should. Also Changed the screen size when the navbar gets display of none, did this because I wanted more spacing between the nav items. Added js for the other websites link and button so the button works to open and close the other websites dropdown menu and so when the page loads the dropdown is hidden. Also needed to move the navigation bar margin to the left so it wasn't overlapping the button, which is why the button yesterday wasn't working, so I am glad I solved that issue lol.

--- javascript/js_main.js
+++ javascript/js_main.js
@@ -1,13 +1,12 @@
-// This hides the navigation bar and shows the navigation button 
-// when the user sizes the window too 500px 
+// This hides the navigation bar and shows the navigation button
+// when the user sizes the window too 500px
 
 document.addEventListener("DOMContentLoaded", function () {
   function hideNavBar() {
-    if (window.innerWidth <= 500) {
+    if (window.innerWidth <= 544) {
       document.getElementById("navbar").style.display = "none";
       document.getElementById("mobile_nav_btn").style.display = "block";
       document.getElementById("dropdown_nav").style.display = "none";
-
     } else {
       document.getElementById("navbar").style.display = "block";
       document.getElementById("mobile_nav_btn").style.display = "none";
@@ -31,9 +30,9 @@ function openNav() {
 }
 
 // Close the navigation menu when clicking anywhere on the window
-document.addEventListener("click", function(event) {
+document.addEventListener("click", function (event) {
   var dropdownNav = document.getElementById("dropdown_nav");
-  var dropdownBtn = document.getElementById("mobile_nav_btn")
+  var dropdownBtn = document.getElementById("mobile_nav_btn");
   // Check if the clicked element is not part of the navigation menu or the mobile navigation button
   if (!dropdownNav || !dropdownBtn.contains(event.target)) {
     dropdownNav.style.display = "none";
@@ -49,19 +48,32 @@ function closeForm() {
   document.getElementsByClassName("contact-form")[0].style.display = "none";
 }
 
+// This hides the other website links dropdown menu when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  var otherSites = document.getElementsByClassName("dropdown-menu1")[0];
+  otherSites.style.display = "none";
+});
 
+// This toggles other websites links dropdown menu
+function openOtherSiteNav() {
+  var dropdownNav = document.getElementsByClassName("dropdown-menu1")[0];
+  if (dropdownNav.style.display === "none") {
+    dropdownNav.style.display = "block";
+  } else {
+    dropdownNav.style.display = "none";
+  }
+}
 
-// this is so the other websites dropdown menu can close when the user clicks anywhere on the page 
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-btn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-menu");
+// this is so the other websites dropdown menu can close when the user clicks anywhere on the page
+window.onclick = function (event) {
+  if (!event.target.matches(".dropdown-btn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu1");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
 };
-
